e61e98aae055469084347f0a4993ba85d8fd882d
starktk <raulsurf.rodrigues@gmail.com>
2023-09-08 03:20:48 -0300

    Repository incompleto

--- app/src/main/java/com/example/ghandapp/login/data/repository/LoginRepository.kt
+++ app/src/main/java/com/example/ghandapp/login/data/repository/LoginRepository.kt
@@ -1,4 +1,28 @@
 package com.example.ghandapp.login.data.repository
 
+import com.example.ghandapp.network.RetrofitNetworkClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class LoginRepository {
+
+    private val client =
+        RetrofitNetworkClient
+            .createNetworkClient()
+            .create(LoginService::class.java)
+
+    suspend fun logar(username: String, password: String): Boolean {
+
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = client.getLog(username, password)
+
+            }
+        }
+    }
+
+
+    private suspend fun svaeUser(): Boolean {
+
+    }
 }
